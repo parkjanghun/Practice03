63e5583fd6bcf9fd3cc2546f88aa5a513978049a
T3AD20 <T3AD20@T3AD20C>
2017-01-10 15:08:13 +0900

    .

--- src/prob06/CalcApp.java
+++ src/prob06/CalcApp.java
@@ -6,10 +6,12 @@ public class CalcApp {
 
 	public static void main(String[] args) {
 		// 힌트: Scanner 와 String 클래스의 split 함수를 사용합니다.
+		
 		Scanner scanner = new Scanner(System.in);
+		
 		while( true ) {
 			/*  코드를 완성 합니다 */
-			
+
 			System.out.print(">>" );
 			String expression = scanner.nextLine();
 			
