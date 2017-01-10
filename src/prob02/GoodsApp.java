c4cd7e1694d2969945f9e28ab7097a57b800f7d9
T3AD20 <T3AD20@T3AD20C>
2017-01-10 15:05:44 +0900

    finished

--- src/prob02/GoodsApp.java
+++ src/prob02/GoodsApp.java
@@ -1,6 +1,36 @@
 package prob02;
 
+import java.util.Scanner;
+
 public class GoodsApp {
+	private static final int COUNT_GOODS = 3;
+	
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner( System.in );
+		
+		Goods[] goods = new Goods[ COUNT_GOODS ];
+		
+		//상품 입력
+		for( int i = 0; i < COUNT_GOODS; i++ ) {
+			String name = scanner.next();
+			int price = scanner.nextInt();
+			int countStock = scanner.nextInt();
+			
+			goods[ i ] = new Goods();
+			goods[ i ].setName(name);
+			goods[ i ].setPrice(price);
+			goods[ i ].setCountStock(countStock);
+		}
+		
+		//상품 정보 출력
+		for( Goods g : goods ) {
+			System.out.println( 
+				g.getName() +
+				"(가격:" + g.getPrice() + ")이 " +
+				g.getCountStock() + "개 입고 되었습니다." );
+		}
+		
+		//자원정리
+		scanner.close();
 	}
 }
--- src/prob06/CalcApp.java
+++ src/prob06/CalcApp.java
@@ -5,15 +5,63 @@ import java.util.Scanner;
 public class CalcApp {
 
 	public static void main(String[] args) {
-		
 		// 힌트: Scanner 와 String 클래스의 split 함수를 사용합니다.
-		
 		Scanner scanner = new Scanner(System.in);
-		
 		while( true ) {
-			
 			/*  코드를 완성 합니다 */
 			
+			System.out.print(">>" );
+			String expression = scanner.nextLine();
+			
+			if( expression.equals( "quit" ) ) {
+				break;
+			}
+			
+			String[] tokens = expression.split( " " );
+			int lValue = Integer.parseInt( tokens[ 0 ] );
+			int rValue = Integer.parseInt( tokens[ 2 ] );
+			switch( tokens[1] ) {
+				case "+" : {
+					Add add = new Add();
+					
+					add.setValue( lValue, rValue );
+					int result = add.calculate();
+					System.out.println( ">>" + result );
+					
+					break;
+				}
+				case "-" : {
+					Sub sub = new Sub();
+					
+					sub.setValue( lValue, rValue );
+					int result = sub.calculate();
+					System.out.println( ">>" + result );
+					
+					break;
+				}				
+				case "*" : {
+					Mul mul = new Mul();
+					
+					mul.setValue( lValue, rValue );
+					int result = mul.calculate();
+					System.out.println( ">>" + result );
+					
+					break;
+				}				
+				case "/" : {
+					Div div = new Div();
+					
+					div.setValue( lValue, rValue );
+					int result = div.calculate();
+					System.out.println( ">>" + result );
+					
+					break;
+				}
+				default : {
+					System.out.println( ">>알수없는 연산입니다." );
+					break;
+				}
+			}
 		}
 		
 		scanner.close();
